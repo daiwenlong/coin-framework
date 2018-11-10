cd0e55327a09b581ae5711307bb43d56f6d46a89
daiwenlong <dwl0208@163.com>
2018-11-10 23:15:39 +0800

    ioc

--- src/main/java/com/me/coin/framework/ioc/impl/CoinIocImpl.java
+++ src/main/java/com/me/coin/framework/ioc/impl/CoinIocImpl.java
@@ -3,7 +3,6 @@ package com.me.coin.framework.ioc.impl;
 
 import java.lang.reflect.Field;
 import java.util.List;
-import java.util.Properties;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
